25a2423e8843a53c4ee811416fbf5c1500d2d2a3
AHMADIEN HAFIZH YUSUFI <ahmadienhafizhyusufi@gmail.com>
2025-11-24 10:18:31 +0700

    Hero Section

---
+++ components/Hero.tsx
@@ -0,0 +1,53 @@
+import { GridBackground } from "./ui/GridBackground";
+import { Spotlight } from "./ui/Spotlight";
+import { TextGenerateEffect } from "./ui/TextGenerateEffect";
+import MagicButton from "./ui/MagicButton";
+import { FaLocationArrow } from "react-icons/fa";
+
+const Hero = () => {
+  return (
+    <div className="pb-20 pt-36">
+      <div>
+        <Spotlight
+          className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
+          fill="white"
+        />
+        <Spotlight
+          className="top-10 left-full h-[80vh] w-[50vw]"
+          fill="purple"
+        />
+        <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
+      </div>
+
+      <GridBackground />
+
+      <div className="flex justify-center relative my-20 z-10">
+        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+          <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+            Dynamic Web Magic With Next.js
+          </h2>
+          <TextGenerateEffect
+            className="text-center text-[40px] md:text-5xl lg:text-6xl"
+            words="Transforming Concepts into Seamless User Experiences"
+          />
+
+          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+            Hi, I&apos;m Ahmadien Hafizh Yusufi, a Next .js Developer based in
+            Jakarta.
+          </p>
+          <a href="#about">
+            <MagicButton
+              title="Show my Work"
+              icon={<FaLocationArrow />}
+              position="right"
+              handleClick={undefined}
+              otherClasses={undefined}
+            />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
