6c3fcd04847f24bd461b9f0f527997b456d92a51
AHMADIEN HAFIZH YUSUFI <ahmadienhafizhyusufi@gmail.com>
2025-11-25 16:35:55 +0700

    Bento Grid (Need to Fix)

---
+++ components/Grid.tsx
@@ -0,0 +1,38 @@
+import { gridItems } from "@/data";
+import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
+import { HiOutlineTemplate } from "react-icons/hi";
+
+const Grid = () => {
+  return (
+    <section id="about">
+      <BentoGrid>
+        {gridItems.map(
+          ({
+            id,
+            title,
+            description,
+            className,
+            img,
+            imgClassName,
+            titleClassName,
+            spareImg,
+          }) => (
+            <BentoGridItem
+              id={id}
+              key={id}
+              title={title}
+              description={description}
+              className={className}
+              img={img}
+              imgClassName={imgClassName}
+              titleClassName={titleClassName}
+              spareImg={spareImg}
+            />
+          )
+        )}
+      </BentoGrid>
+    </section>
+  );
+};
+
+export default Grid;
